8838a3114d5afb3dd072dfc4b40e4ec45843e093
dgdiego <dgdiego.gimenez@gmail.com>
2020-06-27 17:31:11 -0300

    Delete wrong project

--- buildings-admin-node-react/app/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const todoListRouter = require('./pages/to-do-list');
-const { appErrorHandler } = require('../middlewares/error-handler');
-
-router.use('/to-do-list', todoListRouter);
-router.use(appErrorHandler);
-
-module.exports = router;
